ad320de74fe1ad38f9c6c53f912ecd9fc01ae245
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2023-03-03 16:26:11 +0200

    Update Validation.h

--- StudentInformation/Headers/Validation.h
+++ StudentInformation/Headers/Validation.h
@@ -1,3 +1,8 @@
 #include <string>
 
 std::string ValidateGroup(string& groupNumber);
+bool ValidateOptions(int& option);
+void ValidateNumberOfCourses(int& numberOfCourses);
+void ValidateCourses(string& course, set<string>& courses);
+void ValidateGrade(string& grade);
+bool ValidateCountOfStudents(string group);
